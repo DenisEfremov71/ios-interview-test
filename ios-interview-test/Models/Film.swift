b390dea349f47dabbe84e6f0282ab9be396d6261
Denis Efremov <efremov@novuscom.net>
2020-01-02 07:46:02 -0800

    Changed the data model to accommodate 1­ to ­many relationship between Film and Category

--- ios-interview-test/Models/Film.swift
+++ ios-interview-test/Models/Film.swift
@@ -21,7 +21,7 @@ struct Film {
     let shortDesc: String
     let duration: Int
     let thumbnailUrl: URL
-    let categoryId: Int
+    let categoryId: [Int]
     let venueId: Int
     
     var state = FilmImageState.new
@@ -52,7 +52,7 @@ struct Film {
                 let decoder = JSONDecoder()
                 do {
                     var films = try decoder.decode([Film].self, from: jsonData)
-                    films = films.filter { $0.categoryId == category}
+                    films = films.filter { $0.categoryId.contains(category) }
                     completion?(.success(films))
                 } catch {
                     completion?(.failure(error))
@@ -85,7 +85,18 @@ extension Film: Decodable {
         let shortDesc: String = try container.decode(String.self, forKey: .shortDesc)
         let duration: Int = try container.decode(Int.self, forKey: .duration)
         let thumbnailUrl: URL = try container.decode(URL.self, forKey: .thumbnailUrl)
-        let categoryId: Int = try container.decode(Int.self, forKey: .category)
+        var categoryId: [Int] = [Int]()
+        
+        // Assumption: for the "category" type we might received Int or [Int] from the JSON payload
+        // so we have to handle the both cases
+        if let intValue = try? container.decode(Int.self, forKey: .category) {
+            categoryId.append(intValue)
+        } else if let intArrayValue = try? container.decode([Int].self, forKey: .category) {
+            categoryId = intArrayValue
+        } else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: container.codingPath, debugDescription: "Invalid JSON for category ID"))
+        }
+        
         let venueId: Int = try container.decode(Int.self, forKey: .venue)
         
         self.init(uid: uid, name: name, shortDesc: shortDesc, duration: duration, thumbnailUrl: thumbnailUrl, categoryId: categoryId, venueId: venueId, state: FilmImageState.new, image: UIImage(named: "Placeholder"))
